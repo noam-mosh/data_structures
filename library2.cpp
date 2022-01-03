1b468b92d991125a0dbdb66a57c0162a107d6992
EdenKonop <eden.konop@gmail.com>
2022-01-03 21:03:31 +0200

    added library class

---
+++ library2.cpp
@@ -0,0 +1,69 @@
+#include"library2.h"
+#include"PlayerManager.h"
+#define MAX_SCALE 200
+using namespace data_structures;
+
+void *Init(int k, int scale){
+    if(k<=0 || scale >MAX_SCALE || scale <=0){
+        return NULL;
+    }
+    return new PlayerManager(k,scale);
+}
+
+StatusType AddPlayer(void *DS, int PlayerID, int GroupID, int score){
+    if (!DS){
+        return INVALID_INPUT;
+    }
+    return ((PlayerManager*)(DS))->AddPlayer(PlayerID,GroupID,score);
+}
+
+StatusType RemovePlayer(void *DS, int PlayerID){
+    if (!DS){
+        return INVALID_INPUT;
+    }
+    return ((PlayerManager*)(DS))->RemovePlayer(PlayerID);
+}
+
+StatusType IncreasePlayerIDLevel(void *DS, int PlayerID, int LevelIncrease){
+    if (!DS){
+        return INVALID_INPUT;
+    }
+    return ((PlayerManager*)(DS))->IncreasePlayerIDLevel(PlayerID,LevelIncrease);
+}
+
+StatusType ChangePlayerIDScore(void *DS, int PlayerID, int NewScore){
+    if (!DS){
+        return INVALID_INPUT;
+    }
+    return ((PlayerManager*)(DS))->ChangePlayerIDScore(PlayerID, NewScore);
+}
+
+StatusType GetPercentOfPlayersWithScoreInBounds(void *DS, int GroupID, int score, int lowerLevel, int higherLevel,
+                                            double * players){
+    if (!DS){
+        return INVALID_INPUT;
+    }
+    return ((PlayerManager*)(DS))->GetPercentOfPlayersWithScoreInBounds(GroupID,score,lowerLevel,higherLevel,players);   
+}
+
+StatusType AverageHighestPlayerLevelByGroup(void *DS, int GroupID, int m, double * level){
+    if (!DS){
+        return INVALID_INPUT;
+    }
+    return ((PlayerManager*)(DS))->AverageHighestPlayerLevelByGroup(GroupID,m,level);
+}
+
+StatusType GetPlayersBound(void *DS, int GroupID, int score, int m,
+                                         int * LowerBoundPlayers, int * HigherBoundPlayers){
+     if (!DS){
+        return INVALID_INPUT;
+    }
+    return ((PlayerManager*)(DS))->GetPlayersBound(GroupID,score,m,LowerBoundPlayers,HigherBoundPlayers);
+}
+
+void Quit(void** DS){
+    if(!DS){
+        return;
+    }
+    delete (PlayerManager*)(*DS);
+}

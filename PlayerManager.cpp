eb5d2c727a84c0c74eae034320649d66970a7f28
EdenKonop <eden.konop@gmail.com>
2022-01-03 21:02:21 +0200

    PlayerManager class

---
+++ PlayerManager.cpp
@@ -0,0 +1,7 @@
+#include "PlayerManager.h"
+using namespace data_structures;
+
+StatusType PlayerManager::MergeGroups(int GroupID1, int GroupID2){
+    //int group1 = groups.Find(GroupID1);
+    //int group2 = groups.Find(GroupID2);
+}
